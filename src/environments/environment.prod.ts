58ada783314bc8878a49f748cc503a5d4499736c
humayunazaz <humayun.azaz@gmail.com>
2017-10-04 17:50:59 +0500

    minor changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyBlGq5AohS8-4S1BKbgquXQY37LvKPEGJQ",
-    authDomain: "portfolio-970b1.firebaseapp.com",
-    databaseURL: "https://portfolio-970b1.firebaseio.com",
-    projectId: "portfolio-970b1",
-    storageBucket: "",
-    messagingSenderId: "442669020766"
-  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,4 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyBlGq5AohS8-4S1BKbgquXQY37LvKPEGJQ",
-    authDomain: "portfolio-970b1.firebaseapp.com",
-    databaseURL: "https://portfolio-970b1.firebaseio.com",
-    projectId: "portfolio-970b1",
-    storageBucket: "",
-    messagingSenderId: "442669020766"
-  }
 };
